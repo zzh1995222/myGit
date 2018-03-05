a8ef9df12d65217a4b4e54f17dd14a7e9729c080
zzh <916725508@qq.com>
2018-03-05 10:57:54 +0800

    mysql数据库操作类

---
+++ Mysql.class.php
@@ -0,0 +1,82 @@
+<?php
+/**
+mysql数据库底操作类
+ */
+
+class Mysql{
+    protected $link; //数据库连接
+    protected $host; //连接主机
+    protected $user; //连接用户
+    protected $password; //连接密码
+    protected $charset; //设定字符集
+    protected $db; //数据库名
+    protected $sql; //上一条执行的sql语句
+
+    public function __construct($db,$host="localhost", $user="root", $password="",$charset="utf8"){
+       $this->connect($host,$user,$password);
+       $this->setCharset($charset);
+       $this->selectDB($db,$this->link);
+    }
+
+    /**
+     * mysql数据库的连接
+     * @param $host 连接主机名称
+     * @param $user 连接用户
+     * @param $password 连接密码
+     */
+    protected function connect($host,$user,$password){
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+        $this->link = @mysql_connect($this->host,$this->user,$this->password);
+    }
+
+    /**
+     * 设定数据库字符集
+     * @param $charset 字符集
+     */
+    protected function setCharset($charset){
+        $this->charset = $charset;
+        $sql = "set names {$this->charset}";
+        $this->query($sql);
+    }
+
+    /**
+     * 选择数据库
+     * @param $db 需要操作数据库名
+     * @param $link 数据库连接
+     */
+    protected function selectDB($db,$link){
+        $this->db = $db;
+        mysql_select_db($this->db,$link);
+    }
+
+    /**
+     * 执行sql语句
+     * @param $sql sql语句
+     */
+    public function query($sql){
+        $this->sql = $sql;
+        return mysql_query($sql);
+    }
+
+    /**
+     * 从指定表返回一条记录
+     * @param $id 需返回记录id
+     * @param $table 操作表名
+     */
+    public function getOne($id,$table){
+        $sql = "select * from {$table} where id=$id";
+        $res = mysql_query($sql);
+        $data = mysql_fetch_assoc($res);
+        return $data;
+    }
+
+    /**
+     * 获取上一条sql语句
+     */
+    public function getLastSql(){
+        return $this->sql;
+    }
+
+}
